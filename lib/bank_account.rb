c18691387109e5f346bbd7c01e1fa666d898fc1b
Andrew Eames <ajerxs@gmail.com>
2020-12-04 21:26:53 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,9 +2,8 @@ class BankAccount
   attr_accessor :name, :balance, :status
 
   def initialize(name)
-    @name = name
+    @name = name.dup.freeze
     @balance = 1000
     @status = "open"
-    @name.freeze
   end
 end
