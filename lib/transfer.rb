deed381add06dde70f278b09ffc61f2f2a956042
Andrew Eames <ajerxs@gmail.com>
2020-12-04 22:38:38 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,12 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :transfer_amount
+
+  def initialize(name, amount)
+    @sender = name
+    @receiver = name
+    @status = "pending"
+    @transfer_amount = amount
+  end
+    
+    
 end
