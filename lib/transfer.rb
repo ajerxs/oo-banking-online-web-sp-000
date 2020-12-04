be5119aedc828d24fa74fc05a383983ac89488d1
Andrew Eames <ajerxs@gmail.com>
2020-12-04 23:34:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,9 +16,9 @@ class Transfer
 
   def execute_transaction
     if sender.balance >= amount && self.status == "pending"
+      @status = "complete"
       sender.balance -= amount
       receiver.balance += amount
-      @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
