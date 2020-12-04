21980bfa4c7668aa28b91e6d05201b0542c98273
Andrew Eames <ajerxs@gmail.com>
2020-12-04 21:50:27 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,10 +1,13 @@
 class BankAccount
-  attr_accessor :name, :balance, :status
-
-  def initialize(name)
-    @name = name
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize
     @balance = 1000
     @status = "open"
-    expect {raise self.name}.to raise_error
+  end
+
+  def name=(name)
+    @name = name
   end
 end
