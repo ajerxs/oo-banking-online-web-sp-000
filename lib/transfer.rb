a90ba6f6332d1892f1816014a4ad8f2a8973fd90
Andrew Eames <ajerxs@gmail.com>
2020-12-05 00:12:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
 
   def execute_transaction
-    if sender.balance >= amount && self.valid?
+    if self.valid?
       sender.balance -= amount
       receiver.balance += amount
       @status = "complete"
